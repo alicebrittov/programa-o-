484592367455096ee498ceb4d52d6e3f0c08840e
alicebrittov <142256048+alicebrittov@users.noreply.github.com>
2023-08-22 17:03:15 -0300

    primeira AC

---
+++ ac1.py
@@ -0,0 +1,26 @@
+a =float(input("Digite o valor de a: "))
+b =float(input("Digite o valor de b: "))
+c =float(input("Digite o valor de c: "))
+delta = b**2 - 4*a*c
+n1 = (-b + delta ** 0.5) / (2*a)
+n2 = (-b - delta ** 0.5) / (2*a)
+print ("O valor do primeiro x: ", n1,"o valor do segundo x é: ", n2)
+
+
+
+ano= int(input("Digite um ano: "))
+print (ano % 4 == 0 and ano % 100!=0 or (not ano % 400!= 0))
+
+
+
+
+      
+      
+
+
+
+
+
+
+
+
